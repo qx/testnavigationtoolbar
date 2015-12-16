6b3f87e21a1f22156af005215dd614c45d8d3f4e
ok <qx.ouyang@gmail.com>
2015-12-16 12:20:28 +0800

    test nav push

---
+++ testNavintab/SecondDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SecondDetailViewController.swift
+//  testNavintab
+//
+//  Created by ok on 15/12/16.
+//  Copyright © 2015年 ok. All rights reserved.
+//
+
+import UIKit
+
+class SecondDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        self.navigationController?.setToolbarHidden(false, animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- testNavintab/SecondViewController.swift
+++ testNavintab/SecondViewController.swift
@@ -20,6 +20,13 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showSecondDetail(sender: AnyObject) {
+        let vc = self.storyboard?.instantiateViewControllerWithIdentifier("SecondDetailViewController") as! SecondDetailViewController
+        let navigationController = UINavigationController(rootViewController: vc)
+        
+        self.presentViewController(navigationController, animated: true, completion: nil)
+//        self.showViewController(vc, sender: self)
+    }
 
 }
 
